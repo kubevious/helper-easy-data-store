7b8139b4ddb2b42a1489da45a98bdb5af5dce8b3
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-12-12 14:39:58 -0800

    added exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { DataStore } from './data-store'
+export { DataStoreTable } from './data-store-table'
+export { DataStoreTableSynchronizer } from './data-store-table-synchronizer'
+
+export { MetaStore } from './meta/meta-store'
+export { MetaTable } from './meta/meta-table'
+export { MetaTableColumn } from './meta/meta-table-column'
+
+export { MySqlDriver, MySqlStatement } from '@kubevious/helper-mysql';
